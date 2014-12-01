cc6e7de2246c0818ad19b0fc6a5646ee6ad19ef7
s9e <s9e.dev@gmail.com>
2014-12-01 14:08:20 +0100

    Updated Travis scripts

--- scripts/travis/installComposer.sh
+++ scripts/travis/installComposer.sh
@@ -3,5 +3,9 @@
 cd $(dirname "$0")
 cd ../..
 
-#composer install --dev -q --no-interaction
-composer require --dev -q --no-interaction "satooshi/php-coveralls:*"
+composer install --dev -q --no-interaction
+
+if [ -n "$COVERAGE" ]
+then
+	composer require --dev -q --no-interaction "satooshi/php-coveralls:*"
+fi
--- scripts/travis/setup.sh
+++ scripts/travis/setup.sh
@@ -2,13 +2,12 @@
 
 cd $(dirname "$0")
 
+echo "Installing Composer dependencies"
+sh -c "./installComposer.sh 2>&1 &" >/dev/null 2>&1 &
+
 # Install Coveralls and XSLCache if we're saving code coverage and disable XDebug otherwise
 if [ -n "$COVERAGE" ]
 then
-	# We run this script detached in the background. It'll finish while tests are running
-	echo "Installing Composer dependencies"
-	sh -c "./installComposer.sh 2>&1 &" >/dev/null 2>&1 &
-
 	# Install Scrutinizer's external code coverage tool
 	echo "Installing Scrutinizer"
 	./installScrutinizer.sh >/dev/null 2>&1 &
