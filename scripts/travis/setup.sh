1a1d0fc3601c57f1eca92cbf882a295c8373fe2a
s9e <s9e.dev@gmail.com>
2014-12-01 14:15:05 +0100

    Updated Travis scripts

--- scripts/travis/setup.sh
+++ scripts/travis/setup.sh
@@ -2,18 +2,14 @@
 
 cd $(dirname "$0")
 
-echo "Installing Composer dependencies"
-sh -c "./installComposer.sh 2>&1 &" >/dev/null 2>&1 &
+echo "Installing Composer"
+./installComposer.sh
 
-# Install Coveralls and XSLCache if we're saving code coverage and disable XDebug otherwise
 if [ -n "$COVERAGE" ]
 then
-	# Install Scrutinizer's external code coverage tool
 	echo "Installing Scrutinizer"
-	./installScrutinizer.sh >/dev/null 2>&1 &
+	./installScrutinizer.sh
 else
 	echo "Removing XDebug"
 	phpenv config-rm xdebug.ini
 fi
-
-wait
