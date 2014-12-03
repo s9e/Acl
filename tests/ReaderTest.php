3580b5abfc0e03463eb0de1214593521342d9871
s9e <s9e.dev@gmail.com>
2014-12-03 04:53:28 +0100

    Added Reader tests

---
+++ tests/ReaderTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace s9e\TextFormatter\Tests;
+
+use PHPUnit_Framework_TestCase;
+use s9e\Acl\Builder;
+
+/**
+* @covers s9e\Acl\Reader
+*/
+class ReaderTest extends PHPUnit_Framework_TestCase
+{
+	/**
+	* @testdox isAllowed('foo') returns true if allow('foo') was called
+	*/
+	public function testGlobalAllowed()
+	{
+		$builder = new Builder;
+		$builder->allow('foo');
+		$acl = $builder->getReader();
+		$this->assertTrue($acl->isAllowed('foo'));
+	}
+
+	/**
+	* @testdox isAllowed('foo') returns false if deny('foo') was called
+	*/
+	public function testGlobalDenied()
+	{
+		$builder = new Builder;
+		$builder->deny('foo');
+		$acl = $builder->getReader();
+		$this->assertFalse($acl->isAllowed('foo'));
+	}
+
+	/**
+	* @testdox isAllowed('foo') returns false if no permissions were set
+	*/
+	public function testGlobalNoPermissions()
+	{
+		$builder = new Builder;
+		$acl = $builder->getReader();
+		$this->assertFalse($acl->isAllowed('foo'));
+	}
+
+	/**
+	* @testdox isAllowed('foo', ['cat' => 1]) returns true if allow('foo') was called
+	*/
+	public function testGlobalAllowsLocal()
+	{
+		$builder = new Builder;
+		$builder->allow('foo');
+		$acl = $builder->getReader();
+		$this->assertTrue($acl->isAllowed('foo', ['cat' => 1]));
+	}
+
+	/**
+	* @testdox isAllowed('foo', ['cat' => 1]) returns true if allow('foo', ['cat' => 1]) was called
+	*/
+	public function testLocalAllowed()
+	{
+		$builder = new Builder;
+		$builder->allow('foo', ['cat' => 1]);
+		$acl = $builder->getReader();
+		$this->assertTrue($acl->isAllowed('foo', ['cat' => 1]));
+	}
+}
